818074f803ead887157b28b69778ad401b29cc19
Sekou Samassa <sksamassa@gmail.com>
2023-11-11 20:43:28 +0500

    Updating LearningGit component file

--- src/LearningGit.js
+++ src/LearningGit.js
@@ -5,6 +5,13 @@ export default function LearingGit() {
     return(
         <div>
             <h1>Getting started with Git and Github</h1>
+            <h3>Git and Github are two different things.</h3>
+            <ul>
+                <li>Git is a vesion control system.</li>
+                <li>git helps you keep track of code changes.</li>
+                <li>Github is a platform and cloud-based service based on Git.</li>
+                <li>Git is used to collaborate on code.</li>
+            </ul>
         </div>
     )
 }
