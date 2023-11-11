1dd03657b037551d817cd83846b8746ba71d41b7
Sekou Samassa <sksamassa@gmail.com>
2023-11-11 20:26:16 +0500

    Creating a new component and insert it in App component

---
+++ src/LearningGit.js
@@ -0,0 +1,10 @@
+import './learningGit.css';
+
+
+export default function LearingGit() {
+    return(
+        <div>
+            <h1>Getting started with Git and Github</h1>
+        </div>
+    )
+}
